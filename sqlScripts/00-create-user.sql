85901571eee0d1172c706c27a3215cd434b06967
Attila Sinka <41182961+sinkaattila97@users.noreply.github.com>
2025-02-24 13:47:06 +0200

    Moved sqlScripts directory

--- sqlScripts/00-create-user.sql
+++
@@ -1,7 +0,0 @@
--- Drop user first if they exist
-DROP USER IF EXISTS 'jobportal'@'%';
-
--- Now create user with prop privileges
-CREATE USER 'jobportal'@'localhost' IDENTIFIED BY 'jobportal';
-
-GRANT ALL PRIVILEGES ON * . * TO 'jobportal'@'localhost';
